4c98454764ddcff01933403dc334003882669049
BrunoBerisso <bruno@tryolabs.com>
2014-07-15 19:19:06 -0300

    Example 1 BFTask - continueWithBlock

--- Promises/ViewController.m
+++ Promises/ViewController.m
@@ -11,25 +11,43 @@
 #import <BFTaskCompletionSource.h>
 
 
+/******************************************************************************************************************************************************/
+/******************************************************************************************************************************************************/
+
+NSString *randomStringOfLenght(int stringLenght) {
+    char data[stringLenght];
+    for (int x = 0; x < stringLenght; data[x++] = (char)('A' + (arc4random_uniform(26))));
+    return [[NSString alloc] initWithBytes:data length:stringLenght encoding:NSUTF8StringEncoding];
+}
+
 
 @interface AsyncStringGen : NSObject
 
 + (BFTask *)generate;
++ (BFTask *)generateAndAppendTo:(NSString *)otherString;
 
 @end
 
 @implementation AsyncStringGen
 
 + (BFTask *)generate {
+    return [self generateAndAppendTo:nil];
+}
+
++ (BFTask *)generateAndAppendTo:(NSString *)otherString {
     BFTaskCompletionSource *completionSource = [BFTaskCompletionSource taskCompletionSource];
     
     int seconds = arc4random_uniform(3);
-    int stringLenght = 5;
+    int stringLenght = 6;
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(seconds * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        char data[stringLenght];
-        for (int x = 0; x < stringLenght; data[x++] = (char)('A' + (arc4random_uniform(26))));
-        [completionSource setResult:[[NSString alloc] initWithBytes:data length:stringLenght encoding:NSUTF8StringEncoding]];
+        NSString *string = randomStringOfLenght(stringLenght);
+        NSString *result = [NSString stringWithFormat:@"%@%@", otherString ? otherString : @"", string];
+        
+        [completionSource setResult:result];
+//        [completionSource setError:[NSError errorWithDomain:@"Promises" code:0 userInfo:@{NSLocalizedDescriptionKey: result}]];
+//        [completionSource setException:[NSException exceptionWithName:@"PromisesException" reason:@"NoReason" userInfo:@{NSLocalizedDescriptionKey: result}]];
+//        [completionSource cancel];
     });
     
     return completionSource.task;
@@ -38,14 +56,13 @@
 @end
 
 
+/******************************************************************************************************************************************************/
+/******************************************************************************************************************************************************/
 
 
-
-
-
-
-
-
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@end
 
 
 @implementation ViewController
@@ -54,7 +71,16 @@
     [super viewDidLoad];
     
     [[AsyncStringGen generate] continueWithBlock:^id(BFTask *task) {
-        NSLog(@"%@", task.result);
+        
+        if (task.error) {
+            NSLog(@"Generation fail with error: %@", task.error);
+        } else if (task.exception) {
+            NSLog(@"Generation fail with exception: %@", task.exception);
+        } else if (task.isCancelled) {
+            NSLog(@"Generation calceled");
+        } else
+            self.resultLabel.text = task.result;
+        
         return nil;
     }];
 }
