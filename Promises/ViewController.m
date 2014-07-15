bd2265fb4c69eb2b4db7e6c87e2f4e07323412d6
BrunoBerisso <bruno@tryolabs.com>
2014-07-15 13:08:45 -0300

    Example1+Promises

--- Promises/ViewController.m
+++ Promises/ViewController.m
@@ -15,13 +15,65 @@ typedef void(^ErrorHandler)(NSError *error);
 
 
 
+typedef enum {
+    PromisePending,
+    PromiseFulfilled,
+    PromiseRejected
+} PromiseState;
+
+
+@interface Promise : NSObject
+@property (nonatomic) PromiseState state;
+
+- (void)setFulfilledWithResult:(id)result;
+- (void)setRejectedWithError:(NSError *)error;
+
+- (Promise *)whenFulfilled:(SuccessHandler)success;
+- (Promise *)whenRejected:(ErrorHandler)error;
+
+@end
+
+
+@implementation Promise
+
+- (id)init {
+    self = [super init];
+    if (self)
+        self.state = PromisePending;
+    
+    return self;
+}
+
+- (Promise *)whenFulfilled:(SuccessHandler)success {
+    return self;
+}
+
+- (Promise *)whenRejected:(ErrorHandler)error {
+    return self;
+}
+
+- (void)setFulfilledWithResult:(id)result {
+    
+}
+
+- (void)setRejectedWithError:(NSError *)error {
+    
+}
+
+@end
+
+
+
+
+
+
 @interface ViewController ()
 
 @end
 
 
 
-@implementation ViewController
+@implementation ViewController (Example1)
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -33,10 +85,48 @@ typedef void(^ErrorHandler)(NSError *error);
     }];
 }
 
-
 - (void)someAsyncTaskOnSuccess:(SuccessHandler)success onError:(ErrorHandler)error {
     
 }
 
+@end
+
+
+
+@implementation ViewController (Example1_Promise)
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    Promise *promisedResult = [self someAsyncTask];
+    
+    [promisedResult whenFulfilled:^(id result) {
+        //Success with result 'result'
+    }];
+    
+    [promisedResult whenRejected:^(NSError *error) {
+        //Fail with error 'error'
+    }];
+    
+    
+    [[[self someAsyncTask] whenFulfilled:^(id result) {
+        //Success with result 'result'
+    }] whenRejected:^(NSError *error) {
+        //Fail with error 'error'
+    }];
+}
+
+- (Promise *)someAsyncTask {
+    Promise *p = [Promise new]; //p.state == PromisePending;
+    
+    [self someAsyncTaskOnSuccess:^(id result) {
+        [p setFulfilledWithResult:result]; //p.state == PromiseFulfilled;
+    } onError:^(NSError *error) {
+        [p setRejectedWithError:error]; //p.state == PromiseRejected;
+    }];
+    
+    return p;
+}
 
 @end
